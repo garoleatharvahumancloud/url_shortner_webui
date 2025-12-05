b999dc8ba6b4d20f7761cadca3985a3219c586d4
Atharva Garole <atharva.garole@humancloud.co.in>
2025-12-05 12:05:30 +0530

    connected axios api calls to backend

--- app/api/interceptor.tsx
+++ app/api/interceptor.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:8081",  // Spring Boot URL
-  withCredentials: true,             // if using cookies
+  withCredentials: false,             // jwt implementation pending
 });
 
 api.interceptors.request.use(
